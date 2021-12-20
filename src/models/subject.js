b43874532858074f0351a9944849c4e216ffb74e
mqthang_18sh <maiquocthangt@gmail.com>
2021-12-20 15:53:38 +0700

    Update

--- src/models/subject.js
+++ src/models/subject.js
@@ -4,19 +4,30 @@ try {
     subject = subject[0].concat(nav, subject[1], subject[2])
     test = subject
     var app;
-    fetch ("../json/post.json").then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        // listBlog = data.listBlogs
-        app = new Vue({
-            el: "#app",
-            template: test,
-            data: {
-                listBlogs: data.listBlogs
-            }
-        })
+    app = new Vue({
+        el: "#app",
+        template: test,
+        data: {
+            listBlogs: [
+                {
+                    "title": "Những kỹ năng cần thiết để đạt kết quả tốt trong kỳ thi IELTS",
+                    "DateCreated": "12-08-2021",
+                    "Author": "MQT"
+                },
+                {
+                    "title": "Những kỹ năng cần thiết để để trở thành một Datascience pro",
+                    "DateCreated": "12-08-2021",
+                    "Author": "MQT"
+                },
+                {
+                    "title": "Những kỹ năng cần thiết để thiết kế một logo",
+                    "DateCreated": "12-08-2021",
+                    "Author": "MQT"
+                }
+            ]
+        }
     })
-
 } catch (err) {
     window.location.href = url;
+    // console.log(err)
 }
