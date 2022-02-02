869c062f00f925aec13a7d84ad017df8f37e67ee
mqthang_18sh <maiquocthangt@gmail.com>
2022-02-02 12:20:29 +0700

    Update

--- src/views/pages/post/content.vue.js
+++ src/views/pages/post/content.vue.js
@@ -3,11 +3,14 @@ var post = [
     <div class="container">`,
         `
         <div class="body">
+            <div class="LiPostHead">
+                <span> {{ post[0].title }} </span>
+            </div>
             <div class="bodyContent">
                 <div id="Content" class="Content" v-html="post[0].content">
                 </div>               
                 <div class="News">
-                    
+                    <h3 style="color:white; text-align: center;">Related Posts</h3>
                 </div>
             </div>
         </div>`
