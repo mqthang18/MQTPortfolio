e7fb6fee8f6b1b94fb7e915193d59bbdaf8549fc
mqthang_18sh <maiquocthangt@gmail.com>
2022-02-02 18:21:49 +0700

    Update

--- src/models/post.js
+++ src/models/post.js
@@ -4,62 +4,107 @@ try {
     var id = url.searchParams.get("id");
     // console.log(id)
     let APIurl = "https://docs.google.com/spreadsheets/d/1uSydLZo2x6dG1tVMuvyTQ1uIT6CvYEOVh1m8dibeKr4/gviz/tq?sheet=Post";
-    var queryStr = 'Select A, B, C, D, E, F, G, H, I, J where A = '+id;
+    var queryStr = 'Select A, B, C, D, E, F, G, H, I, J'
     // console.log(queryStr)
-    const query = encodeURIComponent(queryStr);
+    var query = encodeURIComponent(queryStr);
     // console.log(query);
-    APIurl = APIurl + '&tq=' + query; 
+    APIurl_1 = APIurl + '&tq=' + query; 
     // console.log(APIurl);
 
-
-    fetch (APIurl).then(res=>res.text()).then(rep=>{
+    fetch(APIurl_1).then(res=>res.text()).then(rep=>{
         // console.log(rep)
-        const dataset = JSON.parse(rep.substr(47).slice(0,-2));
-
-        // Define varfable
-        var data = []
-        var dict =  {}
-        var keys = []
-
-        // Get dataset cols and rows
-        var cols = dataset.table.cols
-        var rows = dataset.table.rows
-        // Create list key for dict
-        for (var i = 0; i < Object.keys(cols).length; i++) {
-            keys.push(cols[i].label)
-        }
-
-        // Create list value for dict
-        for (var i = 0; i < Object.keys(rows).length; i++) {
-            // console.log(rows[i].c)
-            var value = {};
-            var el = rows[i].c;
-            for (var j = 0; j < Object.keys(keys).length; j++) {
-                // console.log(keys[j])
-                var lengthEl = Object.keys(el[i]).length;
-                if (el[j] != null) {
-                    value[keys[j]] = el[j].v;
-                    // console.log(el[j].v)
-                } else {
-                    value[keys[j]] = null;
-                    // console.log('Null')
-                    // console.log(el[j].v)
-                }
-                // value[keys[j]] = el[j];
+        const datasetOne = JSON.parse(rep.substr(47).slice(0,-2));
+    
+            // Define varfable
+            var dataOne = []
+            var dict =  {}
+            var keys = []
+    
+            // Get datasetOne cols and rows
+            var cols = datasetOne.table.cols
+            var rows = datasetOne.table.rows
+            // Create list key for dict
+            for (var i = 0; i < Object.keys(cols).length; i++) {
+                keys.push(cols[i].label)
             }
-            // console.log(value)
-            data.push(value)
-        }
-        // console.log(data)
-
-        var test = post[0].concat(nav, post[1], footer, post[2])
-        CallUI(test, data)
+    
+            // Create list value for dict
+            for (var i = 0; i < Object.keys(rows).length; i++) {
+                // console.log(rows[i].c)
+                var value = {};
+                var el = rows[i].c;
+                for (var j = 0; j < Object.keys(keys).length; j++) {
+                    // console.log(keys[j])
+                    var lengthEl = Object.keys(el[i]).length;
+                    if (el[j] != null) {
+                        value[keys[j]] = el[j].v;
+                        // console.log(el[j].v)
+                    } else {
+                        value[keys[j]] = null;
+                        // console.log('Null')
+                        // console.log(el[j].v)
+                    }
+                    // value[keys[j]] = el[j];
+                }
+                // console.log(value)
+                dataOne.push(value)
+            }
+        // Get data two
+        var queryStr = 'Select A, B, C, D, E, F, G, H, I, J where A = '+id;
+        var query = encodeURIComponent(queryStr);
+        APIurl_2 = APIurl + '&tq=' + query; 
+        fetch (APIurl_2).then(res=>res.text()).then(rep=>{
+            // console.log(rep)
+            const datasetTwo = JSON.parse(rep.substr(47).slice(0,-2));
+    
+            // Define varfable
+            var dataTwo = []
+            var dict =  {}
+            var keys = []
+    
+            // Get datasetTwo cols and rows
+            var cols = datasetTwo.table.cols
+            var rows = datasetTwo.table.rows
+            // Create list key for dict
+            for (var i = 0; i < Object.keys(cols).length; i++) {
+                keys.push(cols[i].label)
+            }
+    
+            // Create list value for dict
+            for (var i = 0; i < Object.keys(rows).length; i++) {
+                // console.log(rows[i].c)
+                var value = {};
+                var el = rows[i].c;
+                for (var j = 0; j < Object.keys(keys).length; j++) {
+                    // console.log(keys[j])
+                    var lengthEl = Object.keys(el[i]).length;
+                    if (el[j] != null) {
+                        value[keys[j]] = el[j].v;
+                        // console.log(el[j].v)
+                    } else {
+                        value[keys[j]] = null;
+                        // console.log('Null')
+                        // console.log(el[j].v)
+                    }
+                    // value[keys[j]] = el[j];
+                }
+                // console.log(value)
+                dataTwo.push(value)
+            }
+            // console.log(dataTwo)
+    
+            var test = post[0].concat(nav, post[1], footer, post[2])
+            CallUI(test, dataOne, dataTwo)
+        })
     })
+    
 } catch (err) {
     window.location.href = url
 }
-async function CallUI(VueTemplate /*Vue template*/, PostData /*API*/) {
-    console.log(PostData)
+
+async function CallUI(VueTemplate /*Vue template*/, RelatedPost /*API*/, PostData /*API*/) {
+    // console.log(RelatedPost);
+    // console.log(PostData);
     var app;
     app = new Vue({
         el: "#app",
@@ -69,7 +114,7 @@ async function CallUI(VueTemplate /*Vue template*/, PostData /*API*/) {
             // listBlogs: PageListBlogs,
             Category: category,
             // title: topic,
-            // news: ListNews,
+            news: RelatedPost,
             post: PostData
         },
         methods: {
--- src/views/pages/post/content.vue.js
+++ src/views/pages/post/content.vue.js
@@ -11,6 +11,19 @@ var post = [
                 </div>               
                 <div class="News">
                     <h3 style="color:white; text-align: center;">Related Posts</h3>
+                    <div v-for="n in news" class="NewBlog">
+                        <div class="BlogElement">
+                            <div class="item">
+                                <div class="itemMessage" style="text-align:unset;">
+                                    <h3> {{ n.title }} </h3>
+                                    <div class="BlogNote">
+                                        <p> {{ n.category }} </p>
+                                        <p> {{ n.dateCreated }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
                 </div>
             </div>
         </div>`
