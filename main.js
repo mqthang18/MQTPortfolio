360038b4dc6ecd4b2795924ec0769784a0fe0e29
mqthang_18sh <maiquocthangt@gmail.com>
2022-02-23 23:49:14 +0700

    Update

--- main.js
+++ main.js
@@ -1,229 +1,243 @@
+"use strict";
+var url = window.location.href;
+var url = new URL(url);
+var topic = url.searchParams.get("topic");
+// Get Navtopic 
+let APIurl = "https://docs.google.com/spreadsheets/d/1uSydLZo2x6dG1tVMuvyTQ1uIT6CvYEOVh1m8dibeKr4/gviz/tq?sheet=";
+var queryStr = 'Select C';
+var query = encodeURIComponent(queryStr);
+APIurl = APIurl + 'NavTopic' + '&tq=' + query; 
+fetch (APIurl).then(res => res.text()).then(rep=>{
+    const datasetOne = JSON.parse(rep.substr(47).slice(0,-2));
 
-        var url = window.location.href;
-        var url = new URL(url);
-        var topic = url.searchParams.get("topic");
-        if (topic == 'subject/IT' || topic == 'subject/DA' || topic == 'subject/EL') {
-            // Xóa các DOM trong trang homepage
-            var myobj = document.getElementsByName("homepage");
-            for (var index = myobj.length - 1; index >= 0; index--) {
-                myobj[index].parentNode.removeChild(myobj[index]);
-            }
-
-            var listElement = {
-                0: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/navigation/homepage/navigationbar.css']
-                }],
-                1: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/navigation/otherpages/responsivenavbar.css']
-                }],
-                2: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/footer/footer.css']
-                }],
-                3: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/subject/content.css']
-                }],
-                4: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/blocks/footer.vue.js'],}
-                ],
-                5: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './public/js/homepage/homepage.js'],
-                }],
-                6: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', topic],
-                    2: ['src', './src/views/blocks/navigationbar.vue.js'],
-                }],
-                7: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/pages/subject/content.vue.js'],
-                }],
-                8: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/models/subject.js'],
-                }]
-            }
-
-            for (var i = 0; i < Object.keys(listElement).length; i++) {
-                CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
-            }
-        } else if (topic == 'homepage' || topic == null) {
-            var listElement = {
-                0: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', 'homepage'],
-                    2: ['href', './public/style/homepage/homepage.css']
-                }],
-                1: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', 'homepage'],
-                    2: ['href', './public/style/navigation/homepage/navigationbar.css']
-                }],
-                2: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', 'homepage'],
-                    2: ['href', './public/style/navigation/homepage/responsivenavbar.css']
-                }],
-                3: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/blocks/navigationbar.vue.js'],
-                }],
-                4: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/pages/homepage/homepage.vue.js'],
-                }],
-                5: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './public/js/homepage/homepage.js'],
-                }],
-                6: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/models/homepage.js'],
-                }]
-            }
-
-            for (var i = 0; i < Object.keys(listElement).length; i++) {
-                CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
-            }
-        } else if (topic == 'me') {
-            // Xóa các DOM trong trang homepage
-            var myobj = document.getElementsByName("homepage");
-            for (var index = myobj.length - 1; index >= 0; index--) {
-                myobj[index].parentNode.removeChild(myobj[index]);
-            }
-
-            var listElement = {
-                0: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/navigation/homepage/navigationbar.css']
-                }],
-                1: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/navigation/otherpages/responsivenavbar.css']
-                }],
-                2: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/footer/footer.css']
-                }],
-                3: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/me/content.css']
-                }],
-                4: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/blocks/footer.vue.js'],}
-                ],
-                5: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './public/js/homepage/homepage.js'],
-                }],
-                6: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', topic],
-                    2: ['src', './src/views/blocks/navigationbar.vue.js'],
-                }],
-                7: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/pages/me/content.vue.js'],
-                }],
-                8: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/models/me.js'],
-                }]
-            }
-
-            for (var i = 0; i < Object.keys(listElement).length; i++) {
-                CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
-            }
-        } else if (topic == 'post') {
-             // Xóa các DOM trong trang homepage
-             var myobj = document.getElementsByName("homepage");
-             for (var index = myobj.length - 1; index >= 0; index--) {
-                 myobj[index].parentNode.removeChild(myobj[index]);
-             }
-
-             var listElement = {
-                0: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/navigation/homepage/navigationbar.css']
-                }],
-                1: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/navigation/otherpages/responsivenavbar.css']
-                }],
-                2: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/footer/footer.css']
-                }],
-                3: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/subject/content.css']
-                }],
-                4: ['head', 'link', {
-                    0: ['rel', "stylesheet"],
-                    1: ['name', topic],
-                    2: ['href', './public/style/post/content.css']
-                }],
-                5: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/blocks/footer.vue.js'],}
-                ],
-                6: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './public/js/homepage/homepage.js'],
-                }],
-                7: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', topic],
-                    2: ['src', './src/views/blocks/navigationbar.vue.js'],
-                }],
-                8: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/views/pages/post/content.vue.js'],
-                }],
-                9: ['body', 'script', {
-                    0: ['name', 'homepage'],
-                    1: ['id', 'homepage'],
-                    2: ['src', './src/models/post.js'],
-                }]
-            }
-
-            for (var i = 0; i < Object.keys(listElement).length; i++) {
-                CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
-            }
+    // Define varfable
+    var dataOne = HandleAPI(datasetOne);
+    // console.log(dataOne);
+    if (topic == 'subject/'.concat(dataOne[0].path) || topic == 'subject/'.concat(dataOne[1].path) || topic == 'subject/'.concat(dataOne[2].path)) {
+        // Xóa các DOM trong trang homepage
+        var myobj = document.getElementsByName("homepage");
+        for (var index = myobj.length - 1; index >= 0; index--) {
+            myobj[index].parentNode.removeChild(myobj[index]);
         }
+
+        var listElement = {
+            0: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/navigation/homepage/navigationbar.css']
+            }],
+            1: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/navigation/otherpages/responsivenavbar.css']
+            }],
+            2: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/footer/footer.css']
+            }],
+            3: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/subject/content.css']
+            }],
+            4: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/blocks/footer.vue.js'],}
+            ],
+            5: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './public/js/homepage/homepage.js'],
+            }],
+            6: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', topic],
+                2: ['src', './src/views/blocks/navigationbar.vue.js'],
+            }],
+            7: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/pages/subject/content.vue.js'],
+            }],
+            8: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/models/subject.js'],
+            }]
+        }
+
+        for (var i = 0; i < Object.keys(listElement).length; i++) {
+            CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
+        }
+    } else if (topic == 'homepage' || topic == null) {
+        var listElement = {
+            0: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', 'homepage'],
+                2: ['href', './public/style/homepage/homepage.css']
+            }],
+            1: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', 'homepage'],
+                2: ['href', './public/style/navigation/homepage/navigationbar.css']
+            }],
+            2: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', 'homepage'],
+                2: ['href', './public/style/navigation/homepage/responsivenavbar.css']
+            }],
+            3: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/blocks/navigationbar.vue.js'],
+            }],
+            4: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/pages/homepage/homepage.vue.js'],
+            }],
+            5: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './public/js/homepage/homepage.js'],
+            }],
+            6: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/models/homepage.js'],
+            }]
+        }
+
+        for (var i = 0; i < Object.keys(listElement).length; i++) {
+            CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
+        }
+    } else if (topic == 'me') {
+        // Xóa các DOM trong trang homepage
+        var myobj = document.getElementsByName("homepage");
+        for (var index = myobj.length - 1; index >= 0; index--) {
+            myobj[index].parentNode.removeChild(myobj[index]);
+        }
+
+        var listElement = {
+            0: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/navigation/homepage/navigationbar.css']
+            }],
+            1: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/navigation/otherpages/responsivenavbar.css']
+            }],
+            2: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/footer/footer.css']
+            }],
+            3: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/me/content.css']
+            }],
+            4: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/blocks/footer.vue.js'],}
+            ],
+            5: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './public/js/homepage/homepage.js'],
+            }],
+            6: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', topic],
+                2: ['src', './src/views/blocks/navigationbar.vue.js'],
+            }],
+            7: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/pages/me/content.vue.js'],
+            }],
+            8: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/models/me.js'],
+            }]
+        }
+
+        for (var i = 0; i < Object.keys(listElement).length; i++) {
+            CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
+        }
+    } else if (topic == 'post') {
+         // Xóa các DOM trong trang homepage
+         var myobj = document.getElementsByName("homepage");
+         for (var index = myobj.length - 1; index >= 0; index--) {
+             myobj[index].parentNode.removeChild(myobj[index]);
+         }
+
+         var listElement = {
+            0: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/navigation/homepage/navigationbar.css']
+            }],
+            1: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/navigation/otherpages/responsivenavbar.css']
+            }],
+            2: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/footer/footer.css']
+            }],
+            3: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/subject/content.css']
+            }],
+            4: ['head', 'link', {
+                0: ['rel', "stylesheet"],
+                1: ['name', topic],
+                2: ['href', './public/style/post/content.css']
+            }],
+            5: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/blocks/footer.vue.js'],}
+            ],
+            6: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './public/js/homepage/homepage.js'],
+            }],
+            7: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', topic],
+                2: ['src', './src/views/blocks/navigationbar.vue.js'],
+            }],
+            8: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/views/pages/post/content.vue.js'],
+            }],
+            9: ['body', 'script', {
+                0: ['name', 'homepage'],
+                1: ['id', 'homepage'],
+                2: ['src', './src/models/post.js'],
+            }]
+        }
+
+        for (var i = 0; i < Object.keys(listElement).length; i++) {
+            CreateTag(listElement[i][0], listElement[i][1], listElement[i][2])
+        }
+    }
+})
+
+        
 // Insert tag to render HTML 
         function CreateTag(HtmlPart, Tag, Attr) {
             var tag = document.createElement(Tag);
