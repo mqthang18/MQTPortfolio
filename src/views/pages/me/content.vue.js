81d92179945cf6454f2b3c7142e3bdfaf9a9143f
mqthang_18sh <maiquocthangt@gmail.com>
2022-03-17 09:32:54 +0700

    Update

--- src/views/pages/me/content.vue.js
+++ src/views/pages/me/content.vue.js
@@ -5,11 +5,11 @@ var me = [
         <h1>About me</h1>
         <p>Welcome to my portforlio! My name is Thang. I'm in Long Xuyen, Viet Nam.</p>
         <p>My major is data engineer and biotech.</p>
-        <p>My english is in B level. And I'm trying to impove it for my CV!</p>
+        <p>My english is in B level. And I'm trying to aware of English knowledge to improve my CV!</p>
         <p>I've joined two project in environmental simulation area.</p>
         <ol>
-            <li>Create an model for simulating ecological succession of forest that disturbed by storm. And apply InVEST model in assess quality and risk of habiat!</li>
-            <li>Now! I'm going on next project along with topic!</li>
+            <li>Create an model for simulating ecological succession of forest that disturbed by storm. And apply InVEST model in assess quality and risk of habitat!</li>
+            <li>Now! I'm going on next project belong to the same topic!</li>
         </ol>
     </div>
     `,
