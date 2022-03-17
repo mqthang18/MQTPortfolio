72e2b00431988c90ac955d51e0100861a3912a9a
mqthang_18sh <maiquocthangt@gmail.com>
2022-03-17 10:54:55 +0700

    Update

--- src/views/pages/me/content.vue.js
+++ src/views/pages/me/content.vue.js
@@ -1,11 +1,31 @@
 var me = [
     `<div class="wrapper" id="homepage" name="homepage">
-<div class="container">`, `
+    <div class="container">`, `
     <div class="body">
         <h1>About me</h1>
-        <p>Welcome to my portforlio! My name is Thang. I'm in Long Xuyen, Viet Nam.</p>
+        <h3>Personal info</h3>
+        <p>Welcome to my portfolio! My name is Thang. I'm in Long Xuyen, Viet Nam.</p>
         <p>My major is data engineer and biotech.</p>
-        <p>My english is in B level. And I'm trying to aware of English knowledge to improve my CV!</p>
+        <p>My favourite is studying programming language and solving data problems by math.</p>
+        <hr>
+        <h3>My skill</h3>
+        <ol>
+            <li>English: B level. And I'm trying to aware of English knowledge to improve my CV!</li>
+            <li>IT skill includes: 
+                <ul>
+                    <li>Design UI/UX with some tools like: Adobe Pts, Adobe XD or Figma</li>
+                    <li>Front-end dev: HTML/JS/CSS/JS and framework: VueJS/ReactJS</li>
+                    <li>Back-end dev: PHP - framework: Laravel; Nodejs - framework: ExpressJS</li>
+                    <li>Syntax with database: MySQL, Microsoft Access</li>
+                    <li>Code mobile app: frontend - framework: React native and backend with NodeJS</li>
+                    <li>Data Analyst by R/Python</li>
+                    <li>Build Video with PowerPoint, Adobe Premiere, Adobe Effect</li>
+                    <li>Design architech with Sketchup/AutoCAD</li>
+                </ul>
+            </li>
+        </ol>
+        <hr>
+        <h3>Affiliation</h3
         <p>I've joined two project in environmental simulation area.</p>
         <ol>
             <li>Create an model for simulating ecological succession of forest that disturbed by storm. And apply InVEST model in assess quality and risk of habitat!</li>
