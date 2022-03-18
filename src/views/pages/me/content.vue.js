3f9251d678f77ddcff485469843bd95b9fdcb2b0
mqthang_18sh <maiquocthangt@gmail.com>
2022-03-18 20:49:24 +0700

    Update

--- src/views/pages/me/content.vue.js
+++ src/views/pages/me/content.vue.js
@@ -3,27 +3,36 @@ var me = [
     <div class="container">`, `
     <div class="body">
         <h1>About me</h1>
-        <h3>Personal info</h3>
-        <p>Welcome to my portfolio! My name is Thang. I'm in Long Xuyen, Viet Nam.</p>
-        <p>My major is data engineer and biotech.</p>
-        <p>My favourite is studying programming language and solving data problems by math.</p>
-        <hr>
-        <h3>My skill</h3>
-        <ol>
-            <li>English: B level. And I'm trying to aware of English knowledge to improve my CV!</li>
-            <li>IT skill includes: 
-                <ul>
-                    <li>Design UI/UX with some tools like: Adobe Pts, Adobe XD or Figma</li>
-                    <li>Front-end dev: HTML/JS/CSS/JS and framework: VueJS/ReactJS</li>
-                    <li>Back-end dev: PHP - framework: Laravel; Nodejs - framework: ExpressJS</li>
-                    <li>Syntax with database: MySQL, Microsoft Access</li>
-                    <li>Code mobile app: frontend - framework: React native and backend with NodeJS</li>
-                    <li>Data Analyst by R/Python</li>
-                    <li>Build Video with PowerPoint, Adobe Premiere, Adobe Effect</li>
-                    <li>Design architech with Sketchup/AutoCAD</li>
-                </ul>
-            </li>
-        </ol>
+        <div class="P1">
+        <div class="right">
+            <img src="./public/img/logo/CardImage.png" alt="Personal image">
+        </div>
+        <div class="left">
+            <h3>Personal info</h3>
+            <p>Welcome to my portfolio! My name is Thang. I'm in Long Xuyen, Viet Nam.</p>
+            <p>My major is data engineer and biotech.</p>
+            <p>My favourite is studying programming languages and solving data problems by math.</p>
+            <hr>
+            <h3>Personal skills</h3>
+            <ol>
+                <li>English: B level. And I'm trying to aware of English knowledge to improve my CV!</li>
+                <li>IT skill includes: 
+                    <ul>
+                        <li>Can use competently Linux or Windows, Office tools;</li>
+                        <li>Design UI/UX with some tools like: Adobe Pts, Adobe XD or Figma;</li>
+                        <li>Front-end dev: HTML/JS/CSS/JS and framework: VueJS/ReactJS;</li>
+                        <li>Back-end dev: PHP - framework: Laravel; Nodejs - framework: ExpressJS;</li>
+                        <li>Syntax with database: MySQL, Microsoft Access;</li>
+                        <li>Code mobile app: frontend - framework: React native and backend with NodeJS</li>
+                        <li>Data Analyse by R/Python;</li>
+                        <li>Build Video with PowerPoint, Adobe Premiere, Adobe After Effect;</li>
+                        <li>Design architech with Sketchup/AutoCAD;</li>
+                    </ul>
+                </li>
+            </ol>
+        </div>
+        </div>
+       
         <hr>
         <h3>Affiliation</h3
         <p>I've joined two project in environmental simulation area.</p>
